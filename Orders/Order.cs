29cfe57f8a21deaa951b149c94722969e808f1ce
Samuel Glover <sglove22@charlotte.edu>
2025-11-17 20:43:55 -0500

    Added Order abstract class and OrderStatus enum

--- Orders/Order.cs
+++ Orders/Order.cs
@@ -1,6 +1,29 @@
-﻿namespace Virtual_Trading_Simulator_Project.Orders;
+﻿using Virtual_Trading_Simulator_Project.Orders.AccountingStrategies;
+using Virtual_Trading_Simulator_Project.Orders.TradeStrategies;
+using Virtual_Trading_Simulator_Project.Statistics;
+using Virtual_Trading_Simulator_Project.Tickers;
+using Virtual_Trading_Simulator_Project.Users;
 
-public class Order
+namespace Virtual_Trading_Simulator_Project.Orders;
+
+
+public abstract class Order
 {
+    public readonly string Id;
+    public readonly string OrderType;
+    public readonly Trader Trader;
+    public readonly int Quantity;
+    public readonly decimal Value;
+    public readonly Ticker Security;
+    public readonly IStatistics OrderStats;
     
+    public readonly ITradeStrategy TradeStrategy;
+    public readonly IAccountingStrategy AccountingStrategy;
+    
+    public DateTime Time {get;}
+    public OrderStatus Status {get;}
+
+    public abstract bool Validate();
+
+    public abstract void PlaceOrder();
 }
--- Orders/OrderStatus.cs
+++ Orders/OrderStatus.cs
@@ -2,5 +2,8 @@
 
 public enum OrderStatus
 {
-    
+    Pending,
+    Filled,
+    Canceled,
+    Failed
 }
