6550c4a9cf500bbaaa38e6a8ad846ab4468c840c
Samuel Glover <sglove22@charlotte.edu>
2025-11-19 17:27:12 -0500

    Added ticker skeleton

--- Tickers/Ticker.cs
+++ Tickers/Ticker.cs
@@ -1,6 +1,43 @@
-﻿namespace Virtual_Trading_Simulator_Project.Tickers;
+﻿using Virtual_Trading_Simulator_Project.Statistics;
+
+namespace Virtual_Trading_Simulator_Project.Tickers;
 
 public abstract class Ticker
 {
+    public readonly string Symbol;
+    public readonly string Name;
+    private double _price;
+    private VolatilityParameters _volatility;
+    public IStatistics TickerStatistics;
+
+    public VolatilityParameters GetVolatility()
+    {
+        
+    }
     
+    
+    public VolatilityParameters UpdateVolatility()
+    {
+        
+    }
+
+    public void SetVolatility(VolatilityParameters volatility)
+    {
+        
+    }
+
+    public double GetPrice()
+    {
+        
+    }
+
+    public void SetPrice(double price)
+    {
+        
+    }
+    
+    public double UpdatePrice()
+    {
+        
+    }
 }
