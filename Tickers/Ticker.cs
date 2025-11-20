dca475c6d0cc95ef0562b9a5a7062f4044b32c4f
Samuel Glover <sglove22@charlotte.edu>
2025-11-19 21:03:16 -0500

    Added locks to each function and functionality to several

--- Tickers/Ticker.cs
+++ Tickers/Ticker.cs
@@ -9,35 +9,56 @@ public abstract class Ticker
     private double _price;
     private VolatilityParameters _volatility;
     public IStatistics TickerStatistics;
+    
+    private readonly object _priceLock = new object();
+    private readonly object _volatilityLock = new object();
 
     public VolatilityParameters GetVolatility()
     {
-        
+        lock (_volatilityLock)
+        {
+            return _volatility;
+        }
     }
     
     
     public VolatilityParameters UpdateVolatility()
     {
-        
+        lock (_volatilityLock)
+        {
+            
+        }
     }
 
     public void SetVolatility(VolatilityParameters volatility)
     {
-        
+        lock (_volatilityLock)
+        {
+            _volatility = volatility;
+        }
     }
 
     public double GetPrice()
     {
-        
+        lock (_priceLock)
+        {
+            return _price;
+        }
     }
 
     public void SetPrice(double price)
     {
-        
+        lock (_priceLock)
+        {
+            _price = price;
+        }
     }
     
     public double UpdatePrice()
     {
-        
+        lock (_priceLock)
+        {
+            
+        }
     }
 }
