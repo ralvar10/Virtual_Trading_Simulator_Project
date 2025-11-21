d21f110ba3931e6b2248bc1a982717eface6dbe1
Samuel Glover <sglove22@charlotte.edu>
2025-11-20 19:49:13 -0500

    Added basic UpdateStatistics and UpdatePrice for Ticker

--- Tickers/Ticker.cs
+++ Tickers/Ticker.cs
@@ -8,7 +8,7 @@ public abstract class Ticker
     public readonly string Name;
     private double _price;
     private VolatilityParameters _volatility;
-    public IStatistics TickerStatistics;
+    public TickerStatistics Statistics;
     
     private readonly object _priceLock = new object();
     private readonly object _volatilityLock = new object();
@@ -35,7 +35,15 @@ public abstract class Ticker
     {
         lock (_volatilityLock)
         {
+            double targetVolatility = _volatility.MinVolatility + 
+                (_volatility.MaxVolatility - _volatility.MinVolatility) * (0.5 + _volatility.Sentiment * 0.3);
             
+            _volatility.CurrentVolatility += (targetVolatility - _volatility.CurrentVolatility) * 0.1;
+            
+            _volatility.CurrentVolatility = Math.Clamp(
+                _volatility.CurrentVolatility, _volatility.MinVolatility, _volatility.MaxVolatility);
+            
+            return _volatility;
         }
     }
 
@@ -67,7 +75,37 @@ public abstract class Ticker
     {
         lock (_priceLock)
         {
+            Random random = new Random();
+
+            double randomValue = random.NextDouble();
             
+            // Allows ticker to have movements up or down with a bias towards sentiment 
+            double sentimentBias = (_volatility.Sentiment + 1) / 2; 
+            
+            bool isPositive = randomValue < sentimentBias;
+
+            double movementMagnitude = random.NextDouble() * 0.05;
+            
+            double volatilityScale = _volatility.CurrentVolatility / 0.02; 
+            movementMagnitude *= volatilityScale;
+            
+            double changePercent = movementMagnitude;
+            
+            if (!isPositive)
+            {
+                changePercent = -changePercent;
+            }
+            
+            _price = _price * (1 + changePercent);
+            
+            if (_price < 0.01)
+            {
+                _price = 0.01;
+            }
+            
+            Statistics.UpdateStatistics();
+            
+            return _price;
         }
     }
 }
