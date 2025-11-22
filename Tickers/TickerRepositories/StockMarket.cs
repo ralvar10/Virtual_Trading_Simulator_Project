844a652ade537f92fbe5e4f3fb85b09943b73127
Samuel Glover <sglove22@charlotte.edu>
2025-11-22 17:05:32 -0500

    Added code for StockMarket

--- Tickers/TickerRepositories/StockMarket.cs
+++ Tickers/TickerRepositories/StockMarket.cs
@@ -3,4 +3,46 @@
 public class StockMarket : ITickerRepository
 {
     private List< Ticker > _tickers;
+    private static ITickerRepository? _instance;
+
+    public ITickerRepository GetRepository()
+    {
+        if (_instance == null)
+        {
+            _instance = new StockMarket();
+            _tickers = new List<Ticker>();
+        }
+        return _instance;
+    }
+
+    public List<Ticker> GetTickers()
+    {
+        return _tickers;
+    }
+
+    public Ticker? SearchBySymbol(string symbol)
+    {
+        return _tickers.FirstOrDefault(t => t.Symbol == symbol);
+    }
+
+    public bool AddTicker(Ticker ticker)
+    {
+        if (!_tickers.Contains(ticker))
+        {
+            _tickers.Add(ticker);
+            return true;
+        }
+        
+        return false;
+    }
+
+    public bool RemoveTicker(string symbol)
+    {
+        Ticker? ticker = SearchBySymbol(symbol);
+
+        if (ticker == null)
+            return false;
+        
+        return _tickers.Remove(ticker);
+    }
 }
