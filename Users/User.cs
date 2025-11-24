cad2a9403228cbd4feb5704280ddc13b0fe2c835
Samuel Glover <sglove22@charlotte.edu>
2025-11-23 20:16:46 -0500

    Added User abstract class code

--- Users/User.cs
+++ Users/User.cs
@@ -2,5 +2,23 @@
 
 public abstract class User
 {
+    private string _password;
     
+    public int Id; 
+    public string Username;
+
+    protected User(string username, string password, int id) 
+    {
+        if (username.Length < 1 || password.Length < 1)
+            throw new ArgumentException("Username or password is invalid");
+        
+        _password = password;
+        Username = username;
+        Id = id;
+    }
+
+    public bool Login(string password)
+    {
+        return password == _password;
+    }
 }
