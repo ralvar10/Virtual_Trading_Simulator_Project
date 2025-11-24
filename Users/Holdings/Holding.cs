4ed272501f7a7030638456f4448fd9dec853a7dc
Samuel Glover <sglove22@charlotte.edu>
2025-11-23 19:56:00 -0500

    Added code for Holding Struct

--- Users/Holdings/Holding.cs
+++ Users/Holdings/Holding.cs
@@ -2,5 +2,17 @@
 
 public struct Holding
 {
-    
+    public DateTime Time;
+    public double InitialCost; // Per security as to allow quantity to be decremented without issue
+    public double Quantity; // Made quantity a double to allow for potential partial trades of securities
+    public readonly Ticker HoldingTicker;
+
+    public Holding(Ticker ticker, double quantity)
+    {
+        HoldingTicker = ticker;
+        Quantity = quantity;
+        
+        Time = DateTime.Now;
+        InitialCost = HoldingTicker.GetPrice(); 
+    }
 }
