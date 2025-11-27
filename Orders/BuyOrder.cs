a2174b4b23969a1eeabf24983bc99ec054578b65
Samuel Glover <sglove22@charlotte.edu>
2025-11-26 19:59:45 -0500

    Added Imports to BuyOrder and SellOrder

--- Orders/BuyOrder.cs
+++ Orders/BuyOrder.cs
@@ -1,4 +1,8 @@
-﻿namespace Virtual_Trading_Simulator_Project.Orders;
+﻿using Virtual_Trading_Simulator_Project.Orders.TradeStrategies;
+using Virtual_Trading_Simulator_Project.Tickers;
+using Virtual_Trading_Simulator_Project.Users;
+
+namespace Virtual_Trading_Simulator_Project.Orders;
 
 public class BuyOrder : Order
 {
--- Orders/SellOrder.cs
+++ Orders/SellOrder.cs
@@ -1,4 +1,9 @@
-﻿namespace Virtual_Trading_Simulator_Project.Orders;
+﻿using Virtual_Trading_Simulator_Project.Orders.AccountingStrategies;
+using Virtual_Trading_Simulator_Project.Orders.TradeStrategies;
+using Virtual_Trading_Simulator_Project.Tickers;
+using Virtual_Trading_Simulator_Project.Users;
+
+namespace Virtual_Trading_Simulator_Project.Orders;
 
 public class SellOrder : Order
 {
