d81c96cc35502a33fd53010b3fa8c5eae1f9643f
Samuel Glover <sglove22@charlotte.edu>
2025-11-26 19:59:20 -0500

    Added BuyOrder constructor

--- Orders/BuyOrder.cs
+++ Orders/BuyOrder.cs
@@ -2,5 +2,6 @@
 
 public class BuyOrder : Order
 {
-    
+    public BuyOrder(string id, Trader trader, int quantity, Ticker security, ITradeStrategy tradeStrategy)
+        : base(id, trader, quantity, security, tradeStrategy){}
 }
