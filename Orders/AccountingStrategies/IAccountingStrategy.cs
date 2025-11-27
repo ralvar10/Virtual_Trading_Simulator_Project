a1667d93350ed350167d112214a4cab4bf547a99
Samuel Glover <sglove22@charlotte.edu>
2025-11-26 20:16:09 -0500

    Filled in IAccountingStrategies

--- Orders/AccountingStrategies/IAccountingStrategy.cs
+++ Orders/AccountingStrategies/IAccountingStrategy.cs
@@ -1,6 +1,8 @@
-﻿namespace Virtual_Trading_Simulator_Project.Orders.AccountingStrategies;
+﻿using Virtual_Trading_Simulator_Project.Users.Holdings;
+
+namespace Virtual_Trading_Simulator_Project.Orders.AccountingStrategies;
 
 public interface IAccountingStrategy
 {
-    
+    public List<Holding> SelectHoldings(double amount, List<Holding> holdings);
 }
