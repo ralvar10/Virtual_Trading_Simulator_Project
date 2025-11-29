2b857be04d6a2a1c1dd18f22afa0dd7de9a6ba53
Samuel Glover <sglove22@charlotte.edu>
2025-11-29 15:25:34 -0500

    Added Program outline

--- Program.cs
+++ Program.cs
@@ -1,9 +1,66 @@
-
+using Virtual_Trading_Simulator_Project.FileHandlers;
+using Virtual_Trading_Simulator_Project.Orders;
+using Virtual_Trading_Simulator_Project.Tickers.TickerRepositories;
+using Virtual_Trading_Simulator_Project.TickHandlers;
+using Virtual_Trading_Simulator_Project.Users;
 
 public class Program
 {
+    private List<User> _users;
+    private User _loggedInUser;
+    private TickerFileHandler _tickerFileHandler;
+    private PortfolioFileHandler _portfolioFileHandler;
+    private ITickerRepository _tickerRepo;
+    private ITickHandler _tickHandler;
+    private OrderFactory _orderFactory;
+    
     public static void Main(string[] args)
     {
         
     }
+
+    private void LoadFromFiles()
+    {
+        
+    }
+
+    private User Login()
+    {
+        
+    }
+
+    private void DisplayOrderHistory()
+    {
+        
+    }
+
+    private void DisplayTickers()
+    {
+        
+    }
+        
+    private void DisplayUserStats()
+    {
+        
+    }
+
+    private void PlaceOrder()
+    {
+        
+    }
+
+    private void ManagePendingOrders()
+    {
+        
+    }
+
+    private void ManageTickers()
+    {
+        
+    }
+
+    private void UpdateTickSpeed()
+    {
+        
+    } 
 }
